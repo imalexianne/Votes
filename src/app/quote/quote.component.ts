c49426e3418a4597bf2899ee9e78ab7ee24a3e90
alineuwi <alikimy02@gmail.com>
2019-01-28 11:37:00 +0200

    import class

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Quote} from '../quote'
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+  quotes=[
+    new Quote(1, 'Water is life'),
+    new Quote(2, 'Sun is light'),
+    new Quote(3, 'God is all'),
+  ]
   constructor() { }
 
   ngOnInit() {
