0bc9d13384bd2b4f770ce1c8514bd904a5303e72
alineuwi <alikimy02@gmail.com>
2019-01-28 11:19:24 +0200

    add quote class and pass arguments into the constructor as well as creating new Quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import{Quote}from'./quote'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Votes';
+
+  quotes=[
+    new Quote(1, 'Water is life'),
+    new Quote(2, 'Sun is light'),
+    new Quote(3, 'God is all'),
+  ]
 }
